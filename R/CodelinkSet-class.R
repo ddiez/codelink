4d3641f1fb721d729957689a71fb9b6bbfb3f7d6
p.aboyoun <p.aboyoun@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-07-24 03:16:33 +0000

    Removed scanDate slot from eSet and replaced it with protocolData slot. Also updated eSet objects and bumped package version numbers.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/codelink@40809 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/CodelinkSet-class.R
+++ R/CodelinkSet-class.R
@@ -2,34 +2,6 @@
 #setClass("CodelinkSet", contains="eSet")
 setClass("CodelinkSet", contains="ExpressionSet")
 
-#
-setMethod("initialize", "CodelinkSet",
-function(.Object,
-	#phenoData = new("AnnotatedDataFrame"),
-	#featureData = new("AnnotatedDataFrame"),
-	#experimentData = new("MIAME"),
-	#annotation = character(),
-	
-	#intensity = new("matrix"),
-	background = new("matrix"),
-	flag = new("matrix"),
-	snr = new("matrix"),
-	...
-) {
-	callNextMethod(.Object,
-		#phenoData = phenoData,
-		#featureData = featureData,
-        #experimentData = experimentData,
-        #annotation = annotation,
-
-		#intensity = intensity,
-		background = background,
-		flag = flag,
-		snr = snr,
-		...
-	)
-})
-
 setValidity("CodelinkSet", function(object) {
 	#assayDataValidMembers(assayData(object), c("intensity", "background", 
 	assayDataValidMembers(assayData(object), c("background", "flag", "snr"))
